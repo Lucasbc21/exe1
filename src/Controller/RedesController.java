5e7d1c22f363a33fc6bbf6c29e94262250f02d91
User <User@DESKTOP-BODDSF8>
2022-03-02 01:22:32 -0300

    Atualização

--- src/Controller/RedesController.java
+++ src/Controller/RedesController.java
@@ -1,5 +1,84 @@
 package Controller;
 
-public class RedesController {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
+public class RedesController {
+	
+	public RedesController() {
+		super();
+	}
+      public String os() {
+    	 // Retorno do Sistema Operacional em execução; 
+    	 String os = System.getProperty("os.name");
+    	 String arch = System.getProperty("os.arch");
+    	 String version = System.getProperty("os.version");
+    	 return os + " - v."+ version +" - arch. "+ arch;
+     }
+     public void callProcess(String process) {
+    	 
+    	 try {
+    	 Runtime.getRuntime().exec(process);
+    	 } catch(IOException e) {
+    		String msgErro = e.getMessage();
+    		if(msgErro.contains("740")) {
+    			StringBuffer buffer = new StringBuffer();
+    			buffer.append("cmd /c");
+    			buffer.append(" ");
+    			buffer.append(process);
+    			try {
+    				Runtime.getRuntime().exec(buffer.toString());
+    			} catch(IOException e1) {
+    				e1.printStackTrace();
+    			}
+    			
+    		} else {
+    			System.err.println(msgErro);
+    		}
+    	 }
+    	 
+     }
+     public void readProcess(String process) {
+    	 
+    	 RedesController procController	= new RedesController();
+			String os = procController.os();
+    	 try {
+    		 Process p = Runtime.getRuntime().exec(process);
+    		 InputStream fluxo = p.getInputStream();
+    		 InputStreamReader leitor = new InputStreamReader(fluxo);
+    		 BufferedReader buffer = new BufferedReader(leitor);
+    		 String linha = buffer.readLine();
+    		 while (linha != null) {
+    			 System.out.println(linha);
+    			 linha = buffer.readLine();
+    			 }
+    		 buffer.close();
+    		 leitor.close();
+    		 fluxo.close();
+    	 } catch(IOException e) {
+    		 e.printStackTrace();
+
+    	 }
+     }
+     public void Segundachamada (String process2) {
+    	 try {
+    		 Process p = Runtime.getRuntime().exec(process2);
+    		 InputStream fluxo = p.getInputStream();
+    		 InputStreamReader leitor = new InputStreamReader(fluxo);
+    		 BufferedReader buffer = new BufferedReader(leitor);
+    		 String linha = buffer.readLine();
+    		 while (linha != null) {
+    			 System.out.println(linha);
+    			 linha = buffer.readLine();
+    			 }
+    		 buffer.close();
+    		 leitor.close();
+    		 fluxo.close();
+    	 } catch(IOException e) {
+    		 e.printStackTrace();
+    		 
+    	 }
+     }
 }
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -1,5 +1,33 @@
-package view;
+ package view;
+import javax.swing.JOptionPane;
+import Controller.RedesController;
 
 public class Principal {
 
+	public static void main(String[] args) {
+		int option;
+		do {
+			option = Integer.parseInt(JOptionPane.showInputDialog(
+			"1 - Identificador de Sistema Operacional\n"+
+			"2 - Chamada de configuração de IP\n"+
+			"3 - Chamada de PING\n"+
+			"9 - Finaliza"));
+			RedesController procController	= new RedesController();
+			String os = procController.os();
+		switch(option) {
+		
+		case 1:System.out.println(os); break;
+		
+		case 2:String process = "ipconfig";procController.readProcess(process); 
+		; break;
+		
+		case 3:String process2 = "PING www.google.com.br";procController.readProcess(process2); 
+		; break;
+		
+		case 9: JOptionPane.showMessageDialog(null, "Encerrando.");break;
+
+		default: JOptionPane.showMessageDialog(null, "Codigo invalido tente novamente");break;
+		}
+		}while(option != 9);
+	}
 }
